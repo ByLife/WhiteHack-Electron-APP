549146304bc48d24ef6d101a2c6715662979aa54
ByLife <bylife@bylife.fr>
2022-10-24 10:32:48 +0200

    V1.1

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, session ,BrowserWindow} = require('electron')
+const path = require('path')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
+var App = app;
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+App.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+
+  const filter = {
+    urls: ['*://classic.minecraft.net/*']
+  };
+  session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
+      details.requestHeaders['Origin'] = null;
+      details.headers['Origin'] = null;
+      callback({ requestHeaders: details.requestHeaders })
+  });
+})
+
+App.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-
-  app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+})
