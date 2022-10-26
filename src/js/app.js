23fa68744d32b95fbaf3a517f80afcc693080dda
ByLife <bylife@bylife.fr>
2022-10-26 02:50:09 +0200

    V1.1.3

--- src/js/app.js
+++ src/js/app.js
@@ -126,9 +126,27 @@ window.onload = function () {
   }
   
   new NavigationPage();
-  /* Credit and Thanks:
-  Matrix - Particles.js;
-  SliderJS - Ettrics;
-  Design - Sara Mazal Web;
-  Fonts - Google Fonts
-  */
+  
+  function getScrollHeight(elm){
+    var savedValue = elm.value
+    elm.value = ''
+    elm._baseScrollHeight = elm.scrollHeight
+    elm.value = savedValue
+  }
+  
+  function onExpandableTextareaInput({ target:elm }){
+    // make sure the input event originated from a textarea and it's desired to be auto-expandable
+    if( !elm.classList.contains('autoExpand') || !elm.nodeName == 'TEXTAREA' ) return
+    
+    var minRows = elm.getAttribute('data-min-rows')|0, rows;
+    !elm._baseScrollHeight && getScrollHeight(elm)
+  
+    elm.rows = minRows
+    rows = Math.ceil((elm.scrollHeight - elm._baseScrollHeight) / 16)
+    elm.rows = minRows + rows
+  }
+  
+  
+  // global delegated event listener
+  document.addEventListener('input', onExpandableTextareaInput)
+  
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,60 @@
 const urlParams = new URLSearchParams(window.location.search);
-if(urlParams.has('identifiant')){
-    if(urlParams.get('identifiant') == "") Redirect()
-    var id = urlParams.get('identifiant')
-    fetch("http://localhost:8080/api/auth/" + id,
-    {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    method: "GET"
-})
-.then(function(res){ 
-  res.json().then(user => {
+async function Main(){
+  RedirectByPressingEscape()
+  var userData = await FetchUserData()
+  MessageInput(userData.token)
+  if(userData == null || userData == undefined) console.log(await userData)
+  document.querySelector(".title-bvn").innerHTML = "Bonjour " + userData.FirstName + "<br> Cours #1 - Introduction"
 
-  }) 
-})
-.catch(function(res){ console.log(res) })
-} else Redirect()
+}
 
 var Redirect = () => {
   document.location.href="./../index.html";
 }
+
+var RedirectByPressingEscape = () => {
+  document.addEventListener('keydown', (event) => {
+    if(event.key == "Escape") Redirect()
+  })
+}
+
+var MessageInput = (userToken) => {
+  document.addEventListener('keydown', (event) => {
+    if(event.key == "Control") sendMessage(userToken)
+  })
+}
+
+var FetchUserData = () => {
+  return new Promise((resolve, reject) => {
+    if(urlParams.has('identifiant')){ // Fetch user data from API
+      if(urlParams.get('identifiant') == "") Redirect()
+      var id = urlParams.get('identifiant')
+
+      fetch("http://localhost:8080/api/auth/" + id,
+        {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: "GET"
+      }).then(function(res){ 
+          res.json().then(user => {
+            if(user.error) Redirect()
+            else resolve(user)
+          }) 
+        })
+        .catch(() => Redirect())
+    } else Redirect()
+  })
+}
+
+var sendMessage = (userToken) => {
+  var message = document.querySelector(".textarea-message").value.replace(/\|&;\$%@"<>\(\)\+,/g, "");
+  fetch(`http://localhost:8080/api/messages/${userToken}/${message}`)
+  .then(res => res.json().then(data => {
+    if(data.error) document.querySelector(".message-callback").innerHTML = `<div class="frame"><button class="custom-btn btn-15">Error</button></div>`
+    else document.querySelector(".message-callback").innerHTML = `<div class="frame"><button class="custom-btn btn-15">Sent</button></div>`
+  }))
+  .catch(() => document.querySelector(".message-callback").innerHTML = `<div class="frame"><button class="custom-btn btn-15">Error</button></div>`)
+}
+Main()
